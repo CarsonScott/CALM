81b35bbf19d7bb59fff5c5b18a03e0a0183ef294
Carson Scott <carsonjscott14@gmail.com>
2017-10-12 21:39:42 -0400

    Added Network

--- src/Network.py
+++ src/Network.py
@@ -1,11 +1,9 @@
-from math import sqrt
 e = 2.718281828459
 
-
 class Network:
-	
+
 	def compute(self, x):	
-		return 1/pow(e, 0.5 * (x - 0.1))
+		return 1/(1 + pow(e, (x - self.bsize)))
 
 	def __init__(self, mSize, bSize):
 		self.links = []
@@ -13,14 +11,12 @@ class Network:
 		self.places = []
 		self.msize = mSize
 		self.bsize = bSize
-		self.lrate = 0.0004
-		self.drate = 0.0001
-
+		self.lrate = 0.004
+		self.drate = 0.0009
 		for i in range(mSize):
 			self.links.append([])
 			self.states.append(0)
 			self.places.append(None)
-
 			for j in range(mSize):
 				self.links[i].append(0)
 
@@ -32,11 +28,11 @@ class Network:
 					self.places[i] = None
 					self.states[i] = 0
 				else:
-					d = self.compute(self.places[i])
+					d = self.compute(self.bsize-self.places[i])
 					self.links[i][x] += self.lrate*d
 			else:
 				self.links[i][x] -= self.drate
-			if abs(self.links[i][x]) > 1:
-				self.links[i][x] /= abs(self.links[i][x])
+	
 		self.states[x] = 1
 		self.places[x] = 0
+
