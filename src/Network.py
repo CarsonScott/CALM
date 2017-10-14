74586c84f763e392cb6f842716798ff1031d46bf
Carson Scott <carsonjscott14@gmail.com>
2017-10-14 14:30:54 -0400

    added license

--- src/Network.py
+++ src/Network.py
@@ -1,7 +1,29 @@
-e = 2.718281828459
+#MIT License
+#
+#Copyright (c) 2017 Carson Scott
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
 
 class Network:
-
+	
+	e = 2.718281828459
 	def compute(self, x):	
 		return 1/(1 + pow(e, (x - self.bsize)))
 
@@ -11,7 +33,7 @@ class Network:
 		self.places = []
 		self.msize = msize
 		self.bsize = bsize
-		self.lrate = 0.002
+		self.lrate = 0.001
 		self.drate = 0.0001
 		for i in range(msize):
 			self.links.append([])
